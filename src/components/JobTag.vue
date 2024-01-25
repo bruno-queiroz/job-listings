e9fee119230e3cccfa6487a94630ae1ee667e459
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-01-25 07:46:23 -0300

    Create JobTag component

---
+++ src/components/JobTag.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+    tag: string
+}>()
+</script>
+
+<template>
+    <div class="tag">
+        {{tag}}
+    </div>
+</template>
+
+<style scoped>
+.tag{
+    padding: .5rem;
+    border-radius: 5px;
+    color: hsl(180, 29%, 50%);
+    background-color: hsl(180, 52%, 96%);
+}
+</style>
